b8a10c81fd819b686b952fc18005a5a94c2b16a4
tanmay4l <mrspot67@gmail.com>
2026-01-21 23:53:18 +0530

    fixed test

--- wincode/src/schema/impls.rs
+++ wincode/src/schema/impls.rs
@@ -1553,10 +1553,7 @@ impl<'de, C: ConfigCore> SchemaRead<'de, C> for Duration {
     fn read(reader: &mut impl Reader<'de>, dst: &mut MaybeUninit<Self::Dst>) -> ReadResult<()> {
         let secs = <u64 as SchemaRead<'de, C>>::get(reader)?;
         let nanos = <u32 as SchemaRead<'de, C>>::get(reader)?;
-        if secs
-            .checked_add(u64::from(nanos) / 1_000_000_000)
-            .is_none()
-        {
+        if secs.checked_add(u64::from(nanos) / 1_000_000_000).is_none() {
             return Err(invalid_value("Duration overflow"));
         }
         dst.write(Duration::new(secs, nanos));
